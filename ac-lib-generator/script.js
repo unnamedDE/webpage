9f632a4c73f91e904f1794053c76dc0a5e9269ac
BuildTools <unconfigured@null.spigotmc.org>
2019-06-02 18:51:42 +0200

    Added export/import for aclib-generator

--- ac-lib-generator/script.js
+++ ac-lib-generator/script.js
@@ -1,3 +1,12 @@
+function download(filename,text) {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename)
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
 function alertReset(){
 	swal({   title: "Reset can't be undone!",
     text: "Are you sure to proceed?",
@@ -28,6 +37,15 @@ function helpMcscript(){
 function helpMcfunction(){
 	swal({   title: "Using this generator with vanilla mcfunctions",
     text: "Using this generator with mcfunctions is very simple. First you have to fill in the input/output items. After that you have to give the recipe a unique ID. The function path should look something like this 'packname:folder/subfolder/craft'. Then paste the most left output in a .mcfunction file (This is the file you have to mention in '#ac_lib:advanced_crafter_recipes'). Then create a subfolder named the way you specified it in function path and create 3 mcfunctions named 1, 2 and 3. The second output has to go into 'craft/1', the third in 'craft/2', etc.",
+    type: "info",
+    confirmButtonColor: "#4C36EC",
+    confirmButtonText: "OK",
+    closeOnConfirm: true
+  });
+}
+function helpExport(){
+	swal({   title: "Exporting Recipes",
+    text: "When you click export, there will be a file download which contains the recipe. Drag & Drop the file to import.",
     type: "info",
     confirmButtonColor: "#4C36EC",
     confirmButtonText: "OK",
@@ -211,6 +229,83 @@ function generateInit() {
     itemout2_nbt.value = itemout2_nbt.value.replace(/^{/, "").replace(/}$/, "");
   }
 }
+
+function exportRecipe() {
+  generateInit();
+
+  var item0_id = document.getElementById('item_0_id');
+  var item0_nbt = document.getElementById('item_0_nbt');
+  var item1_id = document.getElementById('item_1_id');
+  var item1_nbt = document.getElementById('item_1_nbt');
+  var item2_id = document.getElementById('item_2_id');
+  var item2_nbt = document.getElementById('item_2_nbt');
+  var item3_id = document.getElementById('item_3_id');
+  var item3_nbt = document.getElementById('item_3_nbt');
+  var item4_id = document.getElementById('item_4_id');
+  var item4_nbt = document.getElementById('item_4_nbt');
+  var item5_id = document.getElementById('item_5_id');
+  var item5_nbt = document.getElementById('item_5_nbt');
+  var item6_id = document.getElementById('item_6_id');
+  var item6_nbt = document.getElementById('item_6_nbt');
+  var item7_id = document.getElementById('item_7_id');
+  var item7_nbt = document.getElementById('item_7_nbt');
+  var item8_id = document.getElementById('item_8_id');
+  var item8_nbt = document.getElementById('item_8_nbt');
+  var item9_id = document.getElementById('item_9_id');
+  var item9_nbt = document.getElementById('item_9_nbt');
+  var item10_id = document.getElementById('item_10_id');
+  var item10_nbt = document.getElementById('item_10_nbt');
+  var item11_id = document.getElementById('item_11_id');
+  var item11_nbt = document.getElementById('item_11_nbt');
+
+  var itemout0_id = document.getElementById('output_item_0_id');
+  var itemout0_nbt = document.getElementById('output_item_0_nbt');
+  var itemout0_count = document.getElementById('output_item_0_count');
+  var itemout1_id = document.getElementById('output_item_1_id');
+  var itemout1_nbt = document.getElementById('output_item_1_nbt');
+  var itemout1_count = document.getElementById('output_item_1_count');
+  var itemout2_id = document.getElementById('output_item_2_id');
+  var itemout2_nbt = document.getElementById('output_item_2_nbt');
+  var itemout2_count = document.getElementById('output_item_2_count');
+
+  var txtMainPath = document.getElementById("main_path").value;
+  var txtFunctionPath = document.getElementById("function_path").value;
+  var txtRecipeId = document.getElementById("receipe_id").value;
+
+  if(txtRecipeId == '') {return}
+
+  var output = {txtMainPath:txtMainPath,txtFunctionPath:txtFunctionPath,txtRecipeId:txtRecipeId,item0:{id:item0_id.value,nbt:item0_nbt.value},item1:{id:item1_id.value,nbt:item1_nbt.value},item2:{id:item2_id.value,nbt:item2_nbt.value},item3:{id:item3_id.value,nbt:item3_nbt.value},item4:{id:item4_id.value,nbt:item4_nbt.value},item5:{id:item5_id.value,nbt:item5_nbt.value},item6:{id:item6_id.value,nbt:item6_nbt.value},item7:{id:item7_id.value,nbt:item7_nbt.value},item8:{id:item8_id.value,nbt:item8_nbt.value},item9:{id:item9_id.value,nbt:item9_nbt.value},item10:{id:item10_id.value,nbt:item10_nbt.value},item11:{id:item11_id.value,nbt:item11_nbt.value},output0:{id:itemout0_id.value,nbt:itemout0_nbt.value,count:itemout0_count.value},output1:{id:itemout1_id.value,nbt:itemout1_nbt.value,count:itemout1_count.value},output2:{id:itemout2_id.value,nbt:itemout2_nbt.value,count:itemout2_count.value}};
+
+  download(txtRecipeId + '-recipe_export.txt', JSON.stringify(output))
+}
+
+function overrideDefault(ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+}
+
+function dropFiles(ev) {
+  let droppedFiles = ev.dataTransfer.files;
+  let fileInput= droppedFiles[0];
+  if(droppedFiles[0].type != 'text/plain' || (/.+?\-recipe_export.txt/.test(droppedFiles[0].name) == false) && ev.shiftKey == false) {return}
+  console.log(ev.dataTransfer.files[0]);
+  // var reader = new FileReader();
+  // reader.onload = function() {
+  // console.log(reader.result)
+  // }
+  // reader.readAsText(fileInput)
+}
+
+/*
+let input = document.getElementById('input_file')
+input.addEventListener('change', function(e) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    console.log(reader.result)
+  }
+  reader.readAsText(input.files[0])
+},false);
+*/
 
 function generateMcscript() {
   generateInit();
