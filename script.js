05704be3cc838dee63784b7c5455028d686fb8a6
BuildTools <unnamedDE.mail@googlemail.com>
2019-07-07 17:20:26 +0200

    Fixed social/contact button being to big

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if(isMobile()) {
+if(isMobile() && (window.innerHeight / 1.5 > window.innerWidth || window.innerWidth / 1.5 > window.innerHeight)) {
   let mobile_style = document.createElement('link');
   mobile_style.rel = "stylesheet";
   mobile_style.href = "mobile.css";
