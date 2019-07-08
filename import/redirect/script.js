94ceae744054590c13eba375aff676a4b1ca83be
BuildTools <unnamedDE.mail@googlemail.com>
2019-07-08 19:44:05 +0200

    Fixed jittering on redirect pages

--- import/redirect/script.js
+++ import/redirect/script.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', () => {
+  let viewport = document.createElement('meta');
+  viewport.name = "viewport";
+  viewport.content = "width=device-width, initial-scale=1.0";
+  document.querySelector('head').appendChild(viewport);
   let style = document.createElement('link');
   style.rel = "stylesheet";
   style.href = "../import/redirect/style.css";
